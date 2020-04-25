fc494a4567b95b3ad2264de3c5ac662b75aaa5a6
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-04-25 14:50:38 +0200

    Add CartVM and Index

---
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Extensions;
+using Uplift.Models;
+using Uplift.Models.ViewModels;
+using Uplift.Utility;
+
+namespace Uplift.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfwork;
+        
+        [BindProperty]
+        public CartViewModel CartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfwork)
+        {
+            _unitOfwork = unitOfwork;
+            
+            CartVM = new CartViewModel()
+            {
+                OrderHeader = new OrderHeader(),
+                ServiceList = new List<Service>()
+            };
+        }
+
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.GetObject<List<int>>(SD.SessionCart) != null)
+            {
+                var sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+
+                foreach(int serviceId in sessionList)
+                {
+                    CartVM.ServiceList.Add(_unitOfwork.Service.GetFirstOrDefault(u => u.Id == serviceId, includeProperties: "Frequency,Category"));
+                }
+            }
+
+            return View(CartVM);
+        }
+    }
+}
