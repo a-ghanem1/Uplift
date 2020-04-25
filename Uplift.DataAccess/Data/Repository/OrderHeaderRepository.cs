6f0f656e7bf233e55fe19ecb43874cbb6783cdde
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-04-25 19:28:56 +0200

    Add Orders Controller

--- Uplift.DataAccess/Data/Repository/OrderHeaderRepository.cs
+++ Uplift.DataAccess/Data/Repository/OrderHeaderRepository.cs
@@ -16,5 +16,12 @@ namespace Uplift.DataAccess.Data.Repository
         {
             _db = db;
         }
+
+        public void ChangeOrderStatus(int orderHeaderId, string status)
+        {
+            var orderFromDb = _db.OrderHeaders.FirstOrDefault(o => o.Id == orderHeaderId);
+            orderFromDb.Status = status;
+            _db.SaveChanges();
+        }
     }
 }
---
+++ Uplift/Areas/Admin/Controllers/OrdersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Utility;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrdersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        public IActionResult GetAllOrders()
+        {
+            return Json(new { data = _unitOfWork.OrderHeader.GetAll() });
+        }
+
+        public IActionResult GetAllPendingOrders()
+        {
+            return Json(new { data = _unitOfWork.OrderHeader.GetAll(filter: o => o.Status == SD.StatusSubmitted) });
+        }
+
+        public IActionResult GetAllApprovedOrders()
+        {
+            return Json(new { data = _unitOfWork.OrderHeader.GetAll(filter: o => o.Status == SD.StatusApproved) });
+        }
+
+        #endregion
+    }
+}
