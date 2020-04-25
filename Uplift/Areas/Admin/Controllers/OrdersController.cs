3b161d5a49f01a7b5a1154f36cc62d3001926b48
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2020-04-25 21:11:47 +0200

    End of order section

--- Uplift/Areas/Admin/Controllers/OrdersController.cs
+++ Uplift/Areas/Admin/Controllers/OrdersController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models.ViewModels;
 using Uplift.Utility;
 
 namespace Uplift.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class OrdersController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -20,6 +22,39 @@ namespace Uplift.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Approve(int id)
+        {
+            var orderFromDb = _unitOfWork.OrderHeader.Get(id);
+            if (orderFromDb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.OrderHeader.ChangeOrderStatus(id, SD.StatusApproved);
+            return View(nameof(Index));
+        }
+
+        public IActionResult Reject(int id)
+        {
+            var orderFromDb = _unitOfWork.OrderHeader.Get(id);
+            if (orderFromDb == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.OrderHeader.ChangeOrderStatus(id, SD.StatusRejected);
+            return View(nameof(Index));
+        }
+
+        public IActionResult Details(int id)
+        {
+            OrderViewModel OrderVM = new OrderViewModel
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(id),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(filter: o => o.OrderHeaderId == id)
+            };
+
+            return View(OrderVM);
         }
 
         #region API CALLS
